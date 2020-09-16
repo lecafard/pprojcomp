c641d288b499b63e68e9aab6c9e81a4959816e3e
Vincent Wong <vincent@vwong.dev>
2020-09-16 18:24:15 +1000

    Add headings to schedule

--- frontend/src/components/schedule/index.tsx
+++ frontend/src/components/schedule/index.tsx
@@ -7,13 +7,7 @@ interface ScheduleProps {
 }
 
 function Schedule({days=7}: ScheduleProps) {
-  const grid = Array.from(Array(days));
-  for (let i = 0; i < days; i++) {
-    grid[i] = [];
-    for (let j = 0; j < 20; j++) {
-      grid[i].push(<div data-row={i} data-col={j} className={styles.cell}></div>);
-    }
-  }
+  const grid = constructScheduleGrid(days);
 
   return (
     <div className={styles.schedule}>
@@ -22,6 +16,32 @@ function Schedule({days=7}: ScheduleProps) {
       })}
     </div>
   );
+}
+
+function constructScheduleGrid(days: number) {
+  const grid = Array.from(Array(days));
+  
+  for (let i = 0; i < days; i++) {
+    grid[i] = [];
+    if (i === 0) {
+      grid[0][0] = <div></div>;
+      for (let j = 1; j < 20; j++) {
+        // TODO: Figure out time logic
+        grid[i].push(<div data-col={i} data-row={j}>Time</div>);
+      }
+    } else {
+      for (let j = 0; j < 20; j++) {
+        if (j === 0) {
+          // TODO: Figure out date logic
+          grid[i].push(<div data-col={i} data-row={j}>Day</div>)
+        } else {
+          grid[i].push(<div data-col={i} data-row={j} className={styles.cell}></div>);
+        }
+      }
+    }
+  }
+
+  return grid;
 }
 
 export default Schedule;
