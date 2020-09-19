5a952d6ce1d3413ad96b4ed8cb0392c0f6d0a6b3
Vincent Wong <vincent@vwong.dev>
2020-09-19 18:32:45 +1000

    Improve column layout code

--- frontend/src/pages/guest/index.tsx
+++ frontend/src/pages/guest/index.tsx
@@ -22,18 +22,26 @@ function GuestPage() {
   if (!eventDetails?.data) return null;
 
   return (
-    <div className={`${style.container} row`} style={{width: "95%"}}>
-      <div className={`${style["schedule-container"]} is-center col`} style={{marginTop: "20px"}}>
-        <Schedule 
-          dates={eventDetails.data.options.type === "day" ? 
-                  constructDays(eventDetails.data.options.dates) : eventDetails.data.options.dates
-          }
-          times={constructTimes(eventDetails.data.options.minTime, eventDetails.data.options.maxTime)}
-        />
+    <div className={`container`}>
+      <div className={`row`}>
+        <h1>
+          {eventDetails.data.name}
+        </h1>
       </div>
 
-      <div className={`col`} style={{height: "100%"}}>
-        a
+      <div className={`row`}>
+        <div className={`is-center col`} style={{maxHeight: "75vh"}}>
+          <Schedule 
+            dates={eventDetails.data.options.type === "day" ? 
+                    constructDays(eventDetails.data.options.dates) : eventDetails.data.options.dates
+            }
+            times={constructTimes(eventDetails.data.options.minTime, eventDetails.data.options.maxTime)}
+          />
+        </div>
+
+        <div className={`col`} style={{height: "100%"}}>
+          a
+        </div>
       </div>
     </div>
   );
