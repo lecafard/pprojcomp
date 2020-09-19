6aca0929cac91852d17842003ef591778aeb961f
Esther Wong <ew@Esthers-MacBook-Air.local>
2020-09-19 23:53:05 +1000

    basic guest meeting list generation

--- frontend/src/pages/manage/list.tsx
+++ frontend/src/pages/manage/list.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+interface ListItemProps {
+    id: string,
+    name: string,
+    location: string
+}
+
+function ListItem(props: ListItemProps) {
+    return (
+        <div>
+        <h2> {props.name} - {props.location} </h2>
+        guest key: {props.id}
+        </div>
+    )
+}
 function ManageListPage() {
-    return (<div></div>)
+    return (<div>
+
+        {JSON.parse(localStorage.getItem("myMeetings"))['meetingsList']
+            .map((meeting) => (
+                <ListItem id={meeting.id} name={meeting.name} location={meeting.location}/>
+              ))}
+
+        </div>)
 }
 
 export default ManageListPage;
